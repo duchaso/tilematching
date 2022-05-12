4936379de60f6852079577aa1218cda04893b419
Danyl Uchen <uchendanyl@gmail.com>
2022-05-12 16:35:38 +0300

    add board generation

--- board.cpp
+++ board.cpp
@@ -1,29 +1,30 @@
 #include "board.h"
 
-#include <QColor>
+#include <utility>
+#include <random>
 
-Board::Board()
+Board::Board() : m_dimension(15)
 {
+    m_colors = {"red", "blue", "yellow", "grey", "green"};
 
+    generateBoard();
 }
 
 int Board::rowCount(const QModelIndex &parent) const
 {
-    return 10;
+    return m_dimension;
 }
 
 int Board::columnCount(const QModelIndex &parent) const
 {
-    return 10;
+    return m_dimension;
 }
 
 QVariant Board::data(const QModelIndex &index, int role) const
 {
-    //temporary color array (DELETE LATER)
-    QColor colors[] = {"red", "blue", "yellow", "black", "green"};
     switch (role) {
     case Qt::DecorationRole:
-        return colors[(index.row()+index.column()) % 5];
+        return m_data[index.row()][index.column()];
     default:
         break;
     }
@@ -34,4 +35,34 @@ QVariant Board::data(const QModelIndex &index, int role) const
 QHash<int, QByteArray> Board::roleNames() const
 {
     return { {Qt::DecorationRole, "colorBall"} };
+}
+
+void Board::generateBoard()
+{
+    Colors temp;
+
+    for(auto& color : m_colors)
+    {
+        for(int i = 0; i < m_dimension * m_dimension / m_colors.size(); ++i)
+        {
+            temp.append(color);
+        }
+    }
+
+    std::random_device rd;
+    auto rng = std::default_random_engine{rd()};
+    std::shuffle(temp.begin(), temp.end(), rng);
+
+    int cnt = 0;
+    for(int i = 0; i < m_dimension; ++i)
+    {
+        Colors v;
+        for(int j = 0; j < m_dimension; ++j)
+        {
+            v.append(temp[cnt]);
+            ++cnt;
+        }
+        m_data.append(v);
+    }
+
 }
